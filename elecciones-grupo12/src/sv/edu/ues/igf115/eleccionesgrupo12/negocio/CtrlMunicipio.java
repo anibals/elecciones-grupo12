9e8647d7c16ff17cf04b47edd06492bafe25b31a
anibalsibrian <anibalsibrian@gmail.com>
2014-11-07 04:44:43 +0000

    guardar municipio

--- elecciones-grupo12/src/sv/edu/ues/igf115/eleccionesgrupo12/negocio/CtrlMunicipio.java
+++ elecciones-grupo12/src/sv/edu/ues/igf115/eleccionesgrupo12/negocio/CtrlMunicipio.java
@@ -2,29 +2,14 @@ package sv.edu.ues.igf115.eleccionesgrupo12.negocio;
 
 import sv.edu.ues.igf115.eleccionesgrupo12.datos.MunicipioDAO;
 import sv.edu.ues.igf115.eleccionesgrupo12.datos.MunicipioPk;
-import sv.edu.ues.igf115.eleccionesgrupo12.dominio.Departamento;
 import sv.edu.ues.igf115.eleccionesgrupo12.dominio.Municipio;
 
 
 public class CtrlMunicipio {
 
 	private MunicipioDAO daoMunicipio=new MunicipioDAO();
-	/*
+	
 	public boolean guardarMunicipio(String iddepto,String idmunicipio,String nombremuni) {
-		
-		Municipio municipio = daoMunicipio.daMunicipioById(idmunicipio);
-		if (municipio == null) {
-			municipio = new Municipio(iddepto, idmunicipio, nombremuni);
-			daoMunicipio.guardaActualiza(municipio);
-			return true;
-		} else
-			return false;
-	}
-	
-	
-	*/
-	
-public boolean guardarMunicipio(String iddepto,String idmunicipio,String nombremuni) {
 		
 		Municipio municipio =new Municipio(); 
 		if (true) {
@@ -36,6 +21,10 @@ public boolean guardarMunicipio(String iddepto,String idmunicipio,String nombrem
 		} else
 			return false;
 	}
+	
+	
+	
+	
 	
 	
 	
