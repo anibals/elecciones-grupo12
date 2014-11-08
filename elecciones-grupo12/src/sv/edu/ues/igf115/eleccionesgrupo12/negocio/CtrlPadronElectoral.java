ea816a61b0e273b4a07d8cb447c13792e8d19d62
anibalsibrian <anibalsibrian@gmail.com>
2014-11-08 09:48:38 +0000

    ///jrv insertar me da otro error y no se que es..alguien revisa si puede ///Padron solo eliminar me da el problema los otros ya digo q ya estan

--- elecciones-grupo12/src/sv/edu/ues/igf115/eleccionesgrupo12/negocio/CtrlPadronElectoral.java
+++ elecciones-grupo12/src/sv/edu/ues/igf115/eleccionesgrupo12/negocio/CtrlPadronElectoral.java
@@ -1,12 +1,17 @@
 package sv.edu.ues.igf115.eleccionesgrupo12.negocio;
 
+
+import java.math.BigDecimal;
 import java.text.ParseException;
 import java.util.Date;
 
 import sv.edu.ues.igf115.eleccionesgrupo12.datos.PadronElectoralDAO;
+import sv.edu.ues.igf115.eleccionesgrupo12.datos.PartidoPoliticoDAO;
 import sv.edu.ues.igf115.eleccionesgrupo12.datos.UrnaDAO;
 import sv.edu.ues.igf115.eleccionesgrupo12.dominio.PadronElectoral;
+import sv.edu.ues.igf115.eleccionesgrupo12.dominio.PartidoPolitico;
 import sv.edu.ues.igf115.eleccionesgrupo12.dominio.Urna;
+import sv.edu.ues.igf115.eleccionesgrupo12.dominio.Votacion;
 
 
 
@@ -40,10 +45,30 @@ private PadronElectoralDAO daoPadronElectoral=new PadronElectoralDAO();
 			return false;
 	}
 	
+	public PadronElectoral daPadron(String dui) {
+		return daoPadronElectoral.daPadronElectoralById(dui);
+	}
 	
-	
-	
-	
+	public boolean actualizar(String dui, String nombre,
+		String apellido, Date fechanac,String domicilio,String estado,int idurna){
+		
+		UrnaDAO daoUrna= new UrnaDAO();
+		PadronElectoral padron=daoPadronElectoral.daPadronElectoralById(dui);
+				
+		if (padron != null) {
+			      Urna urna=daoUrna.daUrnaById(idurna);
+				    padron.setNombre(nombre);
+				    padron.setApellidos(apellido);
+				    padron.setFecha_nac(fechanac);
+				    padron.setDomicilio(domicilio);
+				    padron.setEstadoVotacion(estado);
+				    padron.setIdUrna(urna);
+					daoPadronElectoral.guardaActualiza(padron);
+					return true;
+			}else
+				return false;
+		
+			}
 	
 	
 }
